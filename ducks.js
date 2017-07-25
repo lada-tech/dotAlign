a67d1f55d16e833882fba48f531292d9ad526a06
Lada Tochtcheva <lada.toch@gmail.com>
2017-07-25 13:35:01 -0400

    create unit tests

--- ducks.js
+++ ducks.js
@@ -1,50 +1,50 @@
 class Duck {
   constructor(start) {
-    this.x = start.split(" ")[0];
-    this.y = start.split(" ")[1];
-    this.heading = start.split(" ")[2];
+    let [x, y, heading] = start.split(' ');
+    this.x = x;
+    this.y = y;
+    this.heading = heading;
   }
   getCoords() { return `${this.x} ${this.y} ${this.heading}` }
 }
 
-class duckMission {
+class DuckMission {
   constructor(pondSize, duckStart) {
     this.xMax = pondSize[0];
     this.yMax = pondSize[2];
     this.duck = new Duck(duckStart);
   }
   makePTurn() {
-    let pNext = {N: "W", W: "S", S: "E", E: "N"};
+    let pNext = {N: 'W', W: 'S', S: 'E', E: 'N'};
     this.duck.heading = pNext[this.duck.heading];
   }
   makeSTurn() {
-    let sNext = {N: "E", W: "N", S: "W", E: "S"};
+    let sNext = {N: 'E', W: 'N', S: 'W', E: 'S'};
     this.duck.heading = sNext[this.duck.heading];
   }
   moveOneStep() {
-    if(this.duck.heading === "N") this.duck.y ++;
-    if(this.duck.heading === "E") this.duck.x ++;
-    if(this.duck.heading === "S") this.duck.y --;
-    if(this.duck.heading === "W") this.duck.x --;
-    if(this.duck.x > this.xMax || this.duck.y > this.yMax) {
-      alert("Hey, duck, stay in the pond!");
-      return;
+    if (this.duck.heading === 'N') this.duck.y ++;
+    if (this.duck.heading === 'E') this.duck.x ++;
+    if (this.duck.heading === 'S') this.duck.y --;
+    if (this.duck.heading === 'W') this.duck.x --;
+    if (this.duck.x > this.xMax || this.duck.y > this.yMax) {
+      console.log('Hey, duck, stay in the pond!');
     }
   }
   moveTheDuck(instructions) {
-    for(let i=0;i<instructions.length;i++) {
-      switch(instructions[i]) {
-        case "P":
+    for (let i = 0;i < instructions.length;i++) {
+      switch (instructions[i]) {
+        case 'P':
           this.makePTurn();
           break;
-        case "S":
+        case 'S':
           this.makeSTurn();
           break;
-        case "F":
+        case 'F':
           this.moveOneStep();
           break;
         default:
-          alert("Invalid instructions");
+          console.log('Invalid instructions');
       }
     }
     return this.duck.getCoords();
@@ -52,17 +52,20 @@ class duckMission {
 }
 
 function mission(pond, firstDuckStart, instructions1, secondDuckStart, instructions2) {
-  let firstDuckMission = new duckMission(pond,firstDuckStart);
+  let firstDuckMission = new DuckMission(pond, firstDuckStart);
   let firstDuckCoords = firstDuckMission.moveTheDuck(instructions1);
 
-  let secondDuckMission = new duckMission(pond,secondDuckStart);
+  let secondDuckMission = new DuckMission(pond, secondDuckStart);
   let secondDuckCoords = secondDuckMission.moveTheDuck(instructions2);
 
-  alert(`${firstDuckCoords}
+  console.log(`${firstDuckCoords}
 ${secondDuckCoords}`);
-  return;
+  return (`${firstDuckCoords}
+${secondDuckCoords}`);
+
 }
 
+export { Duck, DuckMission, mission };
 
 
 
--- test/ducks_test.js
+++ test/ducks_test.js
@@ -1,3 +1,44 @@
-//var expect = require('chai').expect;
+
 import { expect } from 'chai';
-console.log("It's a test");
+import { Duck, DuckMission, mission } from '../ducks.js';
+
+
+describe('Duck', function() {
+  describe('getCoords', function() {
+    it('should return current coordinates and heading', function() {
+      let duck = new Duck('0 0 N');
+      expect(duck.getCoords()).to.equal('0 0 N');
+    });
+  });
+});
+describe('DuckMission', function() {
+
+  describe('makePTurn method', function() {
+    it('should set correct duck heading', function() {
+      const newMission = new DuckMission('3 3', '0 0 N');
+      newMission.makePTurn();
+      expect(newMission.duck.heading).to.equal('W');
+    });
+  });
+  describe('moveOneStep method', function() {
+
+    it('should change coordinate according to duck heading', function() {
+      const newMission = new DuckMission('5 5', '0 0 N');
+      newMission.moveOneStep();
+      expect(newMission.duck.y).to.equal(1);
+    });
+
+  });
+  describe('moveTheDuck method', function() {
+    it('should change coordinates and heading according to instructions', function() {
+      const newMission = new DuckMission('3 3', '0 0 N');
+      expect(newMission.moveTheDuck('FFP')).to.equal('0 2 W');
+    });
+  });
+});
+describe('mission', function() {
+
+  it('should return correct coordinates and heading for both ducks', function() {
+      expect(mission('2 2', '0 0 N', 'FF', '1 1 E', 'FF')).to.equal('0 2 N\n3 1 E');
+    });
+});
